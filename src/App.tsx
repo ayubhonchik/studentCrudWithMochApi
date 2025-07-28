804dfc51678d4299ff0e7d7185b4449a18351f29
ayubhonchik <ayubhonahmadjanov10@gmail.com>
2025-07-28 15:51:56 +0500

    cheked

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,6 @@
 import { useRoutes } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Home from "./pages/Home";
-import Likes from "./pages/Likes";
-import Detail from "./pages/Detail";
-import Login from "./pages/Login";
-
 const App = () => {
   return (
     <>
@@ -19,17 +15,17 @@ const App = () => {
             },
             {
               path: "likes",
-              element: <Likes />,
+              
             },
             {
               path: "detail",
-              element: <Detail />,
+            
             },
           ],
         },
         {
           path: "/login",
-          element: <Login />,
+          
         },
       ])}
     </>
